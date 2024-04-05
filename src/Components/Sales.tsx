8ef446f0cc1e4dbb7df091f2253f3428d3c99f42
chaks64 <shahchintan64@gmail.com>
2024-04-04 17:38:29 -0700

    charts configs

--- src/Components/Sales.tsx
+++ src/Components/Sales.tsx
@@ -1,38 +1,17 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useRef } from "react";
 import { Chart as ChartJS, ChartOptions, registerables } from "chart.js";
-import 'chartjs-adapter-date-fns';
+import "chartjs-adapter-date-fns";
 import { Line } from "react-chartjs-2";
 import { useAppSelector } from "../app/hooks";
 import { getSalesDataByDay } from "../utils/salesData";
 import { SalesContainer } from "../StyledComponents";
 
-const verticalLinePlugin = {
-  id: "verticalLinePlugin",
-  afterDraw: (chart: any) => {
-    if (chart.tooltip._active && chart.tooltip._active.length) {
-      const ctx = chart.ctx;
-      const x = chart.tooltip._active[0].element.x;
-      const topY = chart.scales.y.top;
-      const bottomY = chart.scales.y.bottom;
-
-      ctx.save();
-      ctx.beginPath();
-      ctx.moveTo(x, topY);
-      ctx.lineTo(x, bottomY);
-      ctx.lineWidth = 1;
-      ctx.setLineDash([5, 5]);
-      ctx.strokeStyle = "#007bff";
-      ctx.stroke();
-      ctx.restore();
-    }
-  },
-};
-
 ChartJS.register(...registerables);
-ChartJS.register(verticalLinePlugin);
 
 const Sales: React.FC = () => {
   const salesData = useAppSelector((app) => app.product.sales);
+  const chartRef = useRef(null);
+
 
   const { labels, retailSales, wholesaleSales, maxSales, minSales } = useMemo(
     () => getSalesDataByDay(salesData),
@@ -60,6 +39,7 @@ const Sales: React.FC = () => {
   };
 
   const options: ChartOptions<"line"> = {
+    responsive: true,
     elements: {
       point: {
         radius: 0,
@@ -105,7 +85,7 @@ const Sales: React.FC = () => {
 
   return (
     <SalesContainer>
-      <Line options={options} data={chartData} />
+      <Line ref={chartRef} options={options} data={chartData} />
     </SalesContainer>
   );
 };
